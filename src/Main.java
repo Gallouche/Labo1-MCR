43895ebc954fc9ed4c8283184cacb5da51fb6b01
psrochat <pierre.samuel.rochat@gmail.com>
2017-02-24 17:30:45 +0100

    First

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import views.*;
 
+/**
+ * Created by pierre-samuelrochat on 23.02.17.
+ */
+public class Main {
     public static void main(String[] args) {
-        MainWindow m = new MainWindow();
+
+
+        JFrame f = new JFrame("");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.getContentPane().add(new MainPanel(f), BorderLayout.CENTER);
+        f.pack();
+        f.setVisible(true);
     }
 }
